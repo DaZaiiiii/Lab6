d05adca80013f2212e06330587168d363cd8bcf8
DaZaiiiii <90222734+DaZaiiiii@users.noreply.github.com>
2021-12-28 14:54:18 +0300

    Add files via upload

---
+++ Zaytseva.Lab6.Exercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zaytseva.Lab6.Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Numbers;
+            Numbers = new int[10];
+
+            Random rand = new Random();
+            int i, positive, negaive, nc;
+
+            i = positive = negaive = nc = 0;
+
+            for (i = 0; i < Numbers.Length; i++)
+                Numbers[i] = rand.Next(-5, 5);
+
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                Console.WriteLine(Numbers[i]);
+
+                if (Numbers[i] < 0.0)
+                    nc += Numbers[i];
+
+            }
+
+            Console.WriteLine();
+
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] >= 0.0)
+                    positive++;
+                else
+                    negaive++;
+            }
+
+
+
+            Console.WriteLine("Количество элементов массива >0  = " + positive);
+            Console.WriteLine("Количество элементов массива <0  = " + negaive);
+            Console.WriteLine("Сумма отрицательных элементов массива  = " + nc);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zaytseva.Lab6.Exercise2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Zaytseva.Lab6.Exercise2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int z = 0, x = 0, max = 0;
+
+
+            int[,] k = new int[10, 10]; //вводим матрицу размерами n х n
+            Random r = new Random();
+            Console.WriteLine();
+
+            Console.WriteLine("Исходная матрица:");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    k[i, j] = r.Next(-100, 100); //придаем каждому элементу матрицы случайное значение
+                    Console.Write("{0} ", k[i, j]); //выводи матрицу
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (k[i, j] > max) //находим max-элемент
+                    {
+                        max = k[i, j];
+                        z = i + 1; //присваеваем новой переменной значение i-той строки + 1
+                        x = j + 1; //присваеваем новой переменной значение j-того столбца + 1
+                    }                
+                }
+
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Максимальный элемент матрицы равен " + max);
+
+            Console.WriteLine();
+            Console.WriteLine("Максимальный элемент находится в " + z + " строке и " + x + " столбце");
+            Console.WriteLine();
+
+            k[z - 1, x - 1] = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("{0} ", k[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
